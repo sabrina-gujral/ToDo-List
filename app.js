78cd9158f6de8523ec7134d71856086fb3cac9a2
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-03-02 22:51:12 +0530

    Extract day using getDate()

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-C:\Users\Sabrina\Desktop\Todo-List
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", 'ejs');
+
+app.get('/', function (req, res){
+	var today = new Date();
+	var day = today.toLocaleDateString('en-US', {weekday: 'long'});
+
+	res.render('list', {day: day,})
+})
+
+app.listen(3000, function(req, res){
+	console.log("listening on port 3000...");
+})
