05975a96bd5f4c1d65e796587123cd98bf3b98e0
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-03-02 23:22:56 +0530

    Add new list items dynamically

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = [];
 
 app.get('/', function (req, res){
 	var today = new Date();
-	var day = today.toLocaleDateString('en-US', {weekday: 'long'});
+	var date = today.toDateString().slice(3);
+	var day = today.toLocaleDateString('en-US', {weekday: 'long'}) + ", " + date;
 
-	res.render('list', {day: day,})
+	res.render('list', {day: day,
+		item: items,})
+})
+
+app.post('/', function (req, res) {
+	item = req.body.item;
+	items.push(item);
+	res.redirect('/');
 })
 
 app.listen(3000, function(req, res){
