ec4216c82a8d7794751abbc52e9ea0e647083194
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-03-04 14:36:56 +0530

    Create different urls for weekday, weekend, today

--- app.js
+++ app.js
@@ -8,20 +8,44 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let items = [];
+let weekdayList = [];
+let weekendList = [];
 
-app.get('/', function (req, res){
+app.get('/weekday', function (req, res){
+	res.render('list', {listTitle: "Weekday List",
+		item: weekdayList,});
+} )
+
+app.post('/weekday', function (req, res) {
+	let item = req.body.item;
+	weekdayList.push(item);
+	res.render('list', {listTitle: "Weekday List",
+		item: weekdayList,});
+})
+
+app.get('/weekend', function (req, res){
+	res.render('list', {listTitle: "Weekend List",
+		item: weekendList,});
+} )
+
+app.post('/weekend', function (req, res) {
+	let item = req.body.item;
+	weekendList.push(item);
+	res.redirect('/weekend');
+})
+
+app.get('/today', function (req, res){
 	let today = new Date();
 	let date = today.toDateString().slice(3);
 	let day = today.toLocaleDateString('en-US', {weekday: 'long'}) + ", " + date;
+	res.render('list', {listTitle: day,
+		item: items,});
+} )
 
-	res.render('list', {day: day,
-		item: items,})
-})
-
-app.post('/', function (req, res) {
-	item = req.body.item;
+app.post('/today', function (req, res) {
+	let item = req.body.item;
 	items.push(item);
-	res.redirect('/');
+	res.redirect('/today');
 })
 
 app.listen(3000, function(req, res){
