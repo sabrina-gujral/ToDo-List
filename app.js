53547c43c76c5eeee73ce35e1ce6a01cbc6eb208
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-03-04 21:48:05 +0530

    Clean and beautify code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const date = require((__dirname) + "/date.js");
 const app = express();
 
 app.set("view engine", 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = [];
@@ -13,59 +13,67 @@ let weekdayList = [];
 let weekendList = [];
 
 app.get('/', function(req, res) {
-	res.render('index');
+    res.render('index');
 })
 
-app.post('/', function(req, res){
-	if(req.body.list === 'weekday'){
-		res.redirect('/weekday');
-	} 
-	else if (req.body.list === 'weekend') {
-		res.redirect('/weekend')
-	}
-	else {
-		res.redirect('/today');
-	}
-})
-
-
-app.get('/weekday', function (req, res){
-	res.render('list', {listTitle: "Weekday List",
-		item: weekdayList,});
-} )
-
-app.post('/weekday', function (req, res) {
-	let item = req.body.item;
-	weekdayList.push(item);
-	res.render('list', {listTitle: "Weekday List",
-		item: weekdayList,});
+app.post('/', function(req, res) {
+    if (req.body.list === 'weekday') {
+        res.redirect('/weekday');
+    } else if (req.body.list === 'weekend') {
+        res.redirect('/weekend')
+    } else {
+        res.redirect('/today');
+    }
 })
 
 
-app.get('/weekend', function (req, res){
-	res.render('list', {listTitle: "Weekend List",
-		item: weekendList,});
-} )
+app.get('/weekday', function(req, res) {
+    res.render('list', {
+        listTitle: "Weekday List",
+        item: weekdayList,
+    });
+})
 
-app.post('/weekend', function (req, res) {
-	let item = req.body.item;
-	weekendList.push(item);
-	res.redirect('/weekend');
+app.post('/weekday', function(req, res) {
+	let title = req.body.title;
+	res.write(title);
+    let item = req.body.item;
+    weekdayList.push(item);
+    res.render('list', {
+        listTitle: title,
+        item: weekdayList,
+    });
 })
 
 
-app.get('/today', function (req, res){
-	const day = date.getDate();
-	res.render('list', {listTitle: day,
-		item: items,});
-} )
-
-app.post('/today', function (req, res) {
-	let item = req.body.item;
-	items.push(item);
-	res.redirect('/today');
+app.get('/weekend', function(req, res) {
+    res.render('list', {
+        listTitle: "Weekend List",
+        item: weekendList,
+    });
 })
 
-app.listen(3000, function(req, res){
-	console.log("listening on port 3000...");
+app.post('/weekend', function(req, res) {
+    let item = req.body.item;
+    weekendList.push(item);
+    res.redirect('/weekend');
+})
+
+
+app.get('/today', function(req, res) {
+    const day = date.getDate();
+    res.render('list', {
+        listTitle: day,
+        item: items,
+    });
+})
+
+app.post('/today', function(req, res) {
+    let item = req.body.item;
+    items.push(item);
+    res.redirect('/today');
+})
+
+app.listen(3000, function(req, res) {
+    console.log("listening on port 3000...");
 })
--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 exports.getDate = function() {
-	const today = new Date();
-	const date = today.toDateString().slice(3);
-	const day = today.toLocaleDateString('en-US', {weekday: 'long'}) + ", " + date;
+    const today = new Date();
+    const date = today.toDateString().slice(3);
+    const day = today.toLocaleDateString('en-US', { weekday: 'long' }) + ", " + date;
 
-	return day;
+    return day;
 }
