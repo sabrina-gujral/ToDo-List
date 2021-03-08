f37c76ab65617eab87c7a7a13fff4116735dc68e
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-03-08 18:50:17 +0530

    Add database and change post and get methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require((__dirname) + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,9 +9,35 @@ app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
-let weekdayList = [];
-let weekendList = [];
+mongoose.connect('mongodb://localhost/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsSchema = new mongoose.Schema({
+    name: String,
+    delete: false,
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: 'Welcome to our todo List',
+    delete: false,
+})
+
+const defaultItems = [item1];
+
+const weekdayListSchema = new mongoose.Schema({
+    name: String,
+    delete: false,
+});
+const WeekdayItem = mongoose.model("WeekdayItem", weekdayListSchema);
+
+
+const weekendListSchema = new mongoose.Schema({
+    name: String,
+    delete: false,
+});
+const WeekendItem = mongoose.model("WeekendItem", weekendListSchema);
+
+const weekdayList = [];
 
 app.get('/', function(req, res) {
     res.render('index');
@@ -28,49 +55,97 @@ app.post('/', function(req, res) {
 
 
 app.get('/weekday', function(req, res) {
-    res.render('list', {
-        listTitle: "Weekday List",
-        item: weekdayList,
+    WeekdayItem.find({}, function(err, foundItems) {
+        if (foundItems.length === 0) {
+            WeekdayItem.insertMany(defaultItems, function(err) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("inserted!");
+            });
+            res.redirect('/weekday');
+        }
+        res.render('list', {
+            listTitle: "Weekday List",
+            item: foundItems,
+        });
+
     });
 })
 
 app.post('/weekday', function(req, res) {
-	let title = req.body.title;
-	res.write(title);
-    let item = req.body.item;
-    weekdayList.push(item);
-    res.render('list', {
-        listTitle: title,
-        item: weekdayList,
-    });
+    let title = req.body.title;
+    const itemName = req.body.item;
+    const item = new WeekdayItem({
+        name: itemName,
+        delete: false,
+    })
+
+    item.save();
+    res.redirect('/weekday');
+
 })
 
 
 app.get('/weekend', function(req, res) {
-    res.render('list', {
+    WeekendItem.find({}, function(err, foundItems) {
+        if (foundItems.length === 0) {
+            WeekendItem.insertMany(defaultItems, function(err) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("inserted!");
+            });
+            res.redirect('/weekend');
+        }
+        res.render('list', {
         listTitle: "Weekend List",
-        item: weekendList,
+        item: foundItems,
+    });
+
     });
 })
 
 app.post('/weekend', function(req, res) {
-    let item = req.body.item;
-    weekendList.push(item);
+    const itemName = req.body.item;
+    const item = new WeekendItem({
+        name: itemName,
+        delete: false,
+    })
+
+    item.save();
     res.redirect('/weekend');
 })
 
 
 app.get('/today', function(req, res) {
-    const day = date.getDate();
-    res.render('list', {
-        listTitle: day,
-        item: items,
+    Item.find({}, function(err, foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("inserted!");
+            });
+            res.redirect('/');
+        }
+
+        const day = date.getDate();
+        res.render('list', {
+            listTitle: day,
+            item: foundItems,
+        });
     });
 })
 
 app.post('/today', function(req, res) {
-    let item = req.body.item;
-    items.push(item);
+    const itemName = req.body.item;
+    const item = new Item({
+        name: itemName,
+        delete: false,
+    });
+
+    item.save();
     res.redirect('/today');
 })
 
